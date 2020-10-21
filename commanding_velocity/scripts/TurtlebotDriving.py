af0ca9c88e6866ee27bf1e0e020267db794774dd
Tom Dudley <totallycarbon@gmail.com>
2020-10-21 11:08:28 +0100

    Changed pose theta data to range from 0 to 2pi, completed task 4

--- commanding_velocity/scripts/TurtlebotDriving.py
+++ commanding_velocity/scripts/TurtlebotDriving.py
@@ -4,31 +4,64 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 import tf
 from math import pi
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
 
 class Pose:
+    """Simple Pose Object
+    
+    Contains the values set by odom_callback"""
     def __init__(self,theta,x,y):
         self.theta = theta
         self.x = x
         self.y = y
 
+class Visualisation:
+    """Placeholder Visualisation Class"""
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+        self.xdata, self.ydata = [], []
+        self.ln, = plt.plot([],[], 'ro')
+        self.ani = FuncAnimation(self.fig, self.update, frames=np.linspace(0, 2*np.pi, 128),
+                    init_func=self.init, blit=True)
+        plt.show()
+
+    def init(self):
+        self.ax.set_xlim(0, 2*np.pi)
+        self.ax.set_ylim(-1, 1)
+        return self.ln,
+
+    def update(self,frame):
+        self.xdata.append(frame)
+        self.ydata.append(np.sin(frame))
+        self.ln.set_data(self.xdata, self.ydata)
+        return self.ln,
+
+
 class TurtlebotDriving:
+    """Robot Class
+    
+    Contains methods for both open loop and closed loop control.
+    Publishes to /cmd_vel
+    Subscribes to /odom with the method odom_callback"""
     def __init__(self):
         rospy.init_node('turtlebot_driving', anonymous=True)
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(20)
         self.pose = Pose(0,0,0)
         self.distance = 0
         self.rotation = 0
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.sub = rospy.Subscriber('odom',Odometry,self.odom_callback)
-
-    def atTargetTheta(self, theta, targetTheta):
-        return round(theta,2) == round(targetTheta,2)
+        self.tolerance = -0.05 # tolerated error when turning
+        self.turnThetas = [(pi/2)+self.tolerance, pi+self.tolerance,
+                         (pi+(pi/2))+self.tolerance, (2*pi)+self.tolerance]
 
 
     def open_loop(self):
         while not rospy.is_shutdown():
 
-            for x in range(0,4):
+            for _ in range(0,4):
                 # drive move a distance d
                 move_cmd = Twist()
                 move_cmd.linear.x = 0.15
@@ -51,11 +84,13 @@ class TurtlebotDriving:
 
                 # rotate by angle alpha
                 move_cmd.linear.x = 0.0
-                move_cmd.angular.z = 1.57/2
+                #rotate 90 degrees over two seconds
+                move_cmd.angular.z = (pi/2)/2
                 time = rospy.Time.now()
                 while rospy.Time.now() - time < rospy.Duration(2):
                     self.pub.publish( move_cmd )
                     self.rate.sleep()
+                    print 'My orientation is {}'.format(self.pose.theta)
 
             
             #stop
@@ -65,14 +100,12 @@ class TurtlebotDriving:
     def closed_loop(self):
         while not rospy.is_shutdown():
 
-            for x in range(0,4):
+            for i in range(0,4):
                 # drive move a distance d
                 move_cmd = Twist()
                 move_cmd.linear.x = 0.15
 
                 self.distance = 0
-
-                time = rospy.Time.now()
 
                 oldpose_x = self.pose.x
                 oldpose_y = self.pose.y
@@ -90,14 +123,8 @@ class TurtlebotDriving:
 
                 # rotate by angle alpha
                 move_cmd.linear.x = 0.0
-                move_cmd.angular.z = 1.57/2
-                time = rospy.Time.now()
-                i = pi/2
-                if self.pose.theta + i > pi:
-                    targetTheta = (self.pose.theta + i) - 2*pi
-                else:
-                    targetTheta = self.pose.theta + i
-                while not self.atTargetTheta(self.pose.theta, targetTheta):
+                move_cmd.angular.z = pi/8
+                while self.pose.theta < self.turnThetas[i]:
                     self.pub.publish( move_cmd )
                     self.rate.sleep()
                     print 'My orientation is {}'.format(self.pose.theta)
@@ -112,17 +139,18 @@ class TurtlebotDriving:
                     msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
         (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quarternion)
 
+        if yaw < 0:
+            yaw += 2*pi
         self.pose.theta = yaw
         self.pose.x = msg.pose.pose.position.x
         self.pose.y = msg.pose.pose.position.y
-
-#def plot_trajectory():
 
 
 if __name__ == '__main__':
     try:
         robot = TurtlebotDriving()
         robot.closed_loop()
+        plot = Visualisation()
     except rospy.ROSInterruptException:
         pass
         
